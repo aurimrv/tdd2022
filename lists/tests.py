ff2de359ab7cf2f01b64b2090d520a47d2cbc45e
aurimrv <aurimrv@yahoo.com.br>
2022-05-14 16:29:03 -0300

    Django Test Client assertTemplateUsed

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,5 @@
 from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page
@@ -12,6 +11,5 @@ class HomePageTest(TestCase):
 		self.assertEquals(found.func, home_page)
 
 	def test_home_page_returns_correct_html(self):
-		request = HttpRequest()
-		response = home_page(request)
+		response = self.client.get('/')
 		self.assertTemplateUsed(response, 'home.html')
