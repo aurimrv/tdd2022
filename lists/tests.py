d31a79c0e44a0521b7f80b647673faf548ee198c
aurimrv <aurimrv@yahoo.com.br>
2022-05-21 10:08:38 -0300

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,5 @@
 from django.urls import resolve
 from django.test import TestCase
-from django.template.loader import render_to_string
-
 from lists.views import home_page
 
 class HomePageTest(TestCase):
@@ -13,3 +11,52 @@ class HomePageTest(TestCase):
 	def test_home_page_returns_correct_html(self):
 		response = self.client.get('/')
 		self.assertTemplateUsed(response, 'home.html')
+
+	def test_only_saves_items_when_necessary(self):
+		self.client.get('/')
+		self.assertEquals(Item.objects.count(), 0)
+
+	def test_can_save_a_POST_request(self):
+		self.client.post('/', data={'item_text': 'A new list item'})
+
+		self.assertEquals(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEquals(new_item.text, 'A new list item')
+
+	def test_redirects_after_POST(self):
+		response = self.client.post('/', data={'item_text': 'A new list item'})
+
+		self.assertEquals(response.status_code, 302)
+		self.assertEquals(response['location'], '/')
+
+	def test_displays_all_list_itens(self):
+		Item.objects.create(text='itemey 1')
+		Item.objects.create(text='itemey 2')
+
+		response = self.client.get('/')
+
+		self.assertIn('itemey 1', response.content.decode())
+		self.assertIn('itemey 2', response.content.decode())
+
+
+from lists.models import Item
+
+class ItemModelTest(TestCase):
+
+	def test_saving_and_retriving_items(self):
+		first_item = Item()
+		first_item.text = 'The first (ever) list item'
+		first_item.save()
+
+		second_item = Item()
+		second_item.text = 'Item the second'
+		second_item.save()
+
+		saved_items = Item.objects.all()
+		self.assertEquals(saved_items.count(),2)
+
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+
+		self.assertEquals(first_saved_item.text, 'The first (ever) list item')
+		self.assertEquals(second_saved_item.text, 'Item the second')
